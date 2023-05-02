60a98b4256293d6a98a932570d91897617120bf1
비엔나 <sure@paran.com>
2023-05-02 16:41:33 +0900

    add : DropDownMenu example

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 
+const List<String> list = <String>['One', 'Two', 'Three', 'Four'];
+
 void main() {
   runApp(const MyApp());
 }
@@ -19,14 +21,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(title: Text("flutter App")),
         body: Align(
           alignment: Alignment.center,
-          child: Container(
-            height: double.infinity, width: double.infinity,
-            padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.deepOrangeAccent)
-            ),
-            child: Text("oooo")
-          ),
+          child: DropdownButtonExample(),
 
         ),
 
@@ -42,3 +37,39 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class DropdownButtonExample extends StatefulWidget {
+  const DropdownButtonExample({super.key});
+
+  @override
+  State<DropdownButtonExample> createState() => _DropdownButtonExampleState();
+}
+
+class _DropdownButtonExampleState extends State<DropdownButtonExample> {
+  String dropdownValue = list.first;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: const Icon(Icons.arrow_downward),
+      elevation: 16,
+      style: const TextStyle(color: Colors.deepPurple),
+      underline: Container(
+        height: 2,
+        color: Colors.deepPurpleAccent,
+      ),
+      onChanged: (String? value) {
+        // This is called when the user selects an item.
+        setState(() {
+          dropdownValue = value!;
+        });
+      },
+      items: list.map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+}
