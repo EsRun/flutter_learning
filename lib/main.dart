bb1a31600bb3e28f5892dab7cf9d25799a1f4e1a
비엔나 <sure@paran.com>
2023-05-03 15:50:22 +0900

    add : firebase import

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 const List<String> list = <String>['One', 'Two', 'Three', 'Four'];
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
